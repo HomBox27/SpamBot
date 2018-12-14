bc2cb5e7b2316db67351f6b18186cc93cbc2c63f
HomBox27 <45872408+HomBox27@users.noreply.github.com>
2018-12-14 11:53:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("523085850163019788")
 setInterval(function() {
-channel.send(`#daily`);
+channel.send(`gucci gang gucci gang gucci gang`);
 }, 30)
 })
 
